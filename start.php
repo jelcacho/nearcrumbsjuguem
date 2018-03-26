8b043dce6ea9f239345ee5d25e0bbf24a1076e8c
jelcacho <jelcacho@nearcrumbs.com>
2018-03-26 18:19:59 +0200

    Update start.php

--- start.php
+++ start.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
 <html>
-<body>
-
-<?php
-$servername = "joaquimmysql.mysql.database.azure.com";
-$username = "joaquim@joaquimmysql";
-$password = "!Passw0rd";
-$dbname = "nearcrumbsjuguem";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT id, firstname, lastname FROM myguests";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?> 
-
-</body>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+<?php phpinfo(); ?>
+ </body>
 </html>
